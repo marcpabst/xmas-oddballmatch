99021545b2e13b3727e69272544becefe58f3878
Marc Pabst <mail@marcpabst.de>
2020-10-15 20:30:10 +0200

    ...

--- analysis/configuration.py
+++ analysis/configuration.py
@@ -4,7 +4,7 @@ from ruamel.yaml import YAML
 def load_configuration():
     # Load config
     yaml = YAML(typ='unsafe')
-    with open("../configuration/configuration.yaml") as file:
+    with open("../configuration/configuration2.yaml") as file:
         configuration = yaml.load(file.read())
 
     return configuration
